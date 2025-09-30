d85091be4c44c32c821037a5c8e52805e370e263
timotejtalian <timotej.talian15gmail.com>
2025-09-30 16:16:25 +0200

    a

---
+++ 3.py
@@ -0,0 +1,7 @@
+a=float(input("Zadaj cislo a: "))
+b=float(input("Zadaj cislo b: "))
+x=float(input("Zadaj cislo x: "))
+if a <= x <= b:
+    print("Cislo je z intervalu",a,"az",b)
+else:
+    print("Cislo nie je z intervalu",a,"az",b)
